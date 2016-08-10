ff5a0c285b71673748d54509e14283dcc268d3b0
muratrahmi <muratrahmikurtul@gmail.com>
2016-08-10 21:12:30 +0300

    Add Some Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'ideas#index'
   get 'ideas/new' , to: 'ideas#new'
   get 'ideas' , to: 'ideas#index'
-  resources :ideas
-  root 'ideas#index'
+  get 'ideas/:id' , to: 'ideas#show', as: "idea"
+  post 'ideas' , to: 'ideas#create'
 end
