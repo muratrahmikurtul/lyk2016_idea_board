14854f4dd520ac37f2d53c966bb28f1ae2ffae4a
muratrahmi <muratrahmikurtul@gmail.com>
2016-08-12 12:11:10 +0300

    Add Partial Form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'ideas' , to: 'ideas#index'
   get 'ideas/:id' , to: 'ideas#show', as: "idea"
   post 'ideas' , to: 'ideas#create'
+  get 'ideas/:id/edit', to: 'ideas#edit'
+  put 'ideas/:id', to: 'ideas#update'
+  delete 'ideas/:id', to: 'ideas#destroy'
 end
